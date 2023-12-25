fb8bf502aaeb463a61f9661b0de14f1bb92ec31a
dev-from-cbd <veryfatwombat@gmail.com>
2023-12-26 07:48:48 +1030

    update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function App() {
+  const search = "search";
+
+  return <div className="App">Hello React!)</div>;
+}
+
+export default App;
