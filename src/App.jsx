1d2ec59505d196f92b97684450e2c7d36b8b7160
dev-from-cbd <veryfatwombat@gmail.com>
2023-12-28 06:29:55 +1030

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="title">Hello React!!</h1>
+      <h1 className="title">
+        Cryptocurrency Finder Web App with React JS and Axios
+      </h1>
     </div>
   );
 }
